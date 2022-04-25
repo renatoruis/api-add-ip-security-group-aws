c04f0ea33d4b8bf0dda557f1c09df28da317c5a2
Renato Ruis <renatoruis@pop-os.localdomain>
2022-04-25 17:12:05 -0300

    ajustes script

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exec = require('child_process').exec;
+
+app.get('/', (req, res) => {
+  var ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
+  var cmd = `./scripts/verify-ip.sh ${ip}`;
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      res.send(`Error: ${error}`)
+    }else{
+      res.send(stdout) // Print the public ip address
+    }
+  });
+})
+
+//path to verify if ip is in the list
+app.get('/verify', (req, res) => {
+  var ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
+  var cmd = `./scripts/verify-ip.sh ${ip}`;
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      res.send(`Error: ${error}`)
+    }else{
+      res.send(stdout) // Print the public ip address
+    }
+  });
+})
+
+app.get('/addip', (req, res) => {
+  var ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
+  var cmd = `./scripts/add-ip.sh ${ip}`;
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      res.send(`Error: ${error}`)
+    }else{
+      res.send(stdout) // Print the public ip address
+    }
+  });
+})
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ src/scripts/add-ip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SG_ID='sg-05e2452adb3981e96'
+IP=$1
+
+# Add IP to SG
+aws ec2 authorize-security-group-ingress \
+  --region us-east-1 \
+  --group-id $SG_ID \
+  --protocol tcp \
+  --port 3306 \
+  --cidr $IP/32
+
+echo "IP $IP added to SG $SG_ID"
