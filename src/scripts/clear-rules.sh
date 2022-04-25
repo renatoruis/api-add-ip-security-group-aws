4a11215bc733f31c6206bf678514385c76f10145
Renato Ruis <renatoruis@pop-os.localdomain>
2022-04-25 17:24:20 -0300

    ajustes script

--- src/scripts/clear-rules.sh
+++ src/scripts/clear-rules.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 SG_ID='sg-05e2452adb3981e96'
-IP=$1
-
 
 # Clear Security group rules
 for i in $(aws ec2 describe-security-group-rules --filter Name="group-id",Values="$SG_ID" | jq '.SecurityGroupRules[].SecurityGroupRuleId' -r);
