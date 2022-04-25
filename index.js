d4c5ebfe42e356e820a50c6b1c8a2349611b757d
Renato Ruis <renatoruis@pop-os.localdomain>
2022-04-25 16:58:52 -0300

    ajustes script

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const port = 3000
 const exec = require('child_process').exec;
 
 app.get('/', (req, res) => {
-  const ip = req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
+  var ip = req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
   res.send(`Your IP is: ${ip}`)
 })
 
 //path to verify if ip is in the list
 app.get('/verify', (req, res) => {
-  const ip = req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
-  const cmd = 'verify-ip.sh ' + ip
+  var ip = req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
+  var cmd = 'verify-ip.sh ' + ip
   exec(cmd, (error, stdout, stderr) => {
     if (error) {
       res.send(`Error: ${error}`)
@@ -22,8 +22,8 @@ app.get('/verify', (req, res) => {
 })
 
 app.get('/addip', (req, res) => {
-  const ip = req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
-  const cmd = 'add-ip.sh ' + ip
+  var ip = req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
+  var cmd = 'add-ip.sh ' + ip
   exec(cmd, (error, stdout, stderr) => {
     if (error) {
       res.send(`Error: ${error}`)
