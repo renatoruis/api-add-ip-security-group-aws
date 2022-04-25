35c014a389b4ad11a091db72673a2a02d3a6082d
Renato Ruis <renatoruis@pop-os.localdomain>
2022-04-25 14:33:14 -0300

    ajuste header

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const command =  require('child_process').exec
 
 app.get('/', (req, res) => {
   //get public ip address from request
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var ip = req.headers['X-Real-IP '] || req.connection.remoteAddress;
   res.send(`Your IP is: ${ip}`)
 })
 
 //path to verify if ip is in the list
 app.get('/verify', (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip = req.headers['X-Real-IP '];
   const cmd = `verify-ip.sh ${ip}`
   command(cmd, (err, stdout, stderr) => {
     if (err) {
@@ -25,7 +25,7 @@ app.get('/verify', (req, res) => {
 
 app.get('/addip', (req, res) => {
   // get the ip address from the request
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  const ip = req.headers['X-Real-IP '];
 
   // execute shell command to add the ip to the whitelist
   const cmd = `add-ip-to-sg.sh ${ip}`
